0180bbcf1c4cefe6ed01e5e9acc082838dc0a914
Myagko <118819179+Myagko@users.noreply.github.com>
2023-02-09 04:05:00 +0700

    Add files via upload

---
+++ home/.config/tint2/executors/ws4.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+workspace="4"
+
+window="`wmctrl -l | awk '{print $2}' | grep "^$workspace" | sed '2,$d'`"
+
+eval $(xprop -root -notype _NET_CURRENT_DESKTOP | sed 's, ,,g')
+
+if [ "$_NET_CURRENT_DESKTOP" == $workspace ]
+	then
+		printf "%s\n" 
+	else
+		if [ $window == $workspace ]
+			then
+				printf "%s\n" 
+			else
+				printf "%s\n" 
+			fi
+	fi
