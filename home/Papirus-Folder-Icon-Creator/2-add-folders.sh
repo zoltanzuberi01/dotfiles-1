d84f361a5f3f3473f5d9fc9f07b314bdf7384752
Myagko <118819179+Myagko@users.noreply.github.com>
2023-02-09 04:50:58 +0700

    Update 2-add-folders.sh

--- home/Papirus-Folder-Icon-Creator/2-add-folders.sh
+++ home/Papirus-Folder-Icon-Creator/2-add-folders.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rsync -avhu ~/Desktop/Papirus-Folder-Icon-Creator/New-Icons/22x22/ ~/.icons/Papirus/22x22/places/
-rsync -avhu ~/Desktop/Papirus-Folder-Icon-Creator/New-Icons/24x24/ ~/.icons/Papirus/24x24/places/
-rsync -avhu ~/Desktop/Papirus-Folder-Icon-Creator/New-Icons/32x32/ ~/.icons/Papirus/32x32/places/
-rsync -avhu ~/Desktop/Papirus-Folder-Icon-Creator/New-Icons/48x48/ ~/.icons/Papirus/48x48/places/
-rsync -avhu ~/Desktop/Papirus-Folder-Icon-Creator/New-Icons/64x64/ ~/.icons/Papirus/64x64/places/
+rsync -avhu ~/Papirus-Folder-Icon-Creator/New-Icons/22x22/ ~/.icons/Papirus/22x22/places/
+rsync -avhu ~/Papirus-Folder-Icon-Creator/New-Icons/24x24/ ~/.icons/Papirus/24x24/places/
+rsync -avhu ~/Papirus-Folder-Icon-Creator/New-Icons/32x32/ ~/.icons/Papirus/32x32/places/
+rsync -avhu ~/Papirus-Folder-Icon-Creator/New-Icons/48x48/ ~/.icons/Papirus/48x48/places/
+rsync -avhu ~/Papirus-Folder-Icon-Creator/New-Icons/64x64/ ~/.icons/Papirus/64x64/places/
